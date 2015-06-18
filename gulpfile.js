fa0f446d0354649df325bb8b3d44a69267d9f5ff
张卓斌 <8609546@qq.com>
2015-06-18 21:27:56 +0800

    develop

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp=require('gulp')
+var plumber=require('gulp-plumber')
+var util=require('gulp-util')
+var stylus=require('gulp-stylus')
+var browserSync=require('browser-sync')
+var jade=require('gulp-jade')
+var autoprefixer=require('gulp-autoprefixer')
+var uglify=require('gulp-uglify')
+var webpack=require('gulp-webpack')
+var rename=require('gulp-rename')
+
+var reload=browserSync.reload()
+var error = function(e){
+    util.beep()
+    util.log(e.toString())
+}
+var paths={
+    develop:{
+        jade:"src/jade/*.jade",
+        html:"src/html/**",
+        css:"src/css/**",
+        stylus:"src/stylus/**",
+        js:"src/js/main.js",
+        jsx:"src/jsx/**",
+        img:"src/img/**",
+        data:"src/data/**"
+    },
+    build:{
+        html:"build",
+        css:"build/css",
+        js:"build/js",
+        img:"build/img",
+        data:"build/data"
+    }
+}
+gulp.task('jade', function () {
+    return gulp.src([paths.develop.jade])
+        .pipe(plumber({"errorHandler":error}))
+        .pipe(jade())
+        .pipe(gulp.dest(paths.build.html))
+})
+gulp.task("stylus", function () {
+    return gulp.src([paths.develop.stylus])
+        .pipe(plumber({"errorHandler":error}))
+        .pipe(stylus())
+        .pipe(autoprefixer())
+        //.pipe(minifyCss())
+        .pipe(gulp.dest(paths.build.css))
+})
+gulp.task("jsx",function(){
+    return gulp.src([paths.develop.jsx])
+        .pipe(plumber({"errorHandler":error}))
+        .pipe(webpack(
+            {
+                module: {
+                    loaders: [
+                        { test: /\.json$/, loader: 'json' },
+                        { test: /\.jsx$/, exclude:/node_modules/,loader: 'babel-loader' }
+                    ]
+                },
+                stage: 0
+            }
+        ))
+        .pipe(uglify())
+        .pipe(rename('react-initial-index.min.js'))
+        .pipe(gulp.dest(paths.build.js))
+})
+gulp.task("data",function(){
+    return gulp.src([paths.develop.data])
+        .pipe(gulp.dest(paths.build.data))
+})
+gulp.task("browser-sync", function () {
+    browserSync({
+        server:{
+            baseDir:"build"
+        }
+    })
+    gulp.watch("src/data/**",["data",reload])
+    gulp.watch("src/jade/**",["jade",reload])
+    gulp.watch("src/stylus/**",["stylus"],reload)
+    gulp.watch("src/img/**",["img",reload])
+    gulp.watch("src/jsx/**",["jsx"],reload)
+})
+
+gulp.task("default", function(){
+    gulp.run("jade","jsx","stylus","browser-sync","data");
+})
